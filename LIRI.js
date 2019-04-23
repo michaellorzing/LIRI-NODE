baacb49888b9b97e72f2fba621c66370b7da54f0
michael Lorzing <ghostsofgrace@gmail.com>
2019-04-23 13:34:52 -0400

    complete?

--- LIRI.js
+++ LIRI.js
@@ -27,42 +27,44 @@ switch (request) {
     break;
 }
 
-//  function spotifyThis(){
-//    if(userInput === ""){
-//      spotify.search({type: "track", query: "The Sign"}, function(data){
-//       console.log("Artist: " + data.tracks.items[0].artists[0].name);
-//       console.log("Title: " + data.tracks.items[0].name);
-//       console.log("Preview: " + data.tracks.items[0].preview_url);
-//       console.log("Album: " + data.tracks.items[0].album.name);
-//      })
-//    } else {
-//      spotify.search({type: "track", query: `${userInput}`}, function(data){
-//       console.log("Artist: " + data.tracks.items[0].artists[0].name);
-//       console.log("Title: " + data.tracks.items[0].name);
-//       console.log("Preview: " + data.tracks.items[0].preview_url);
-//       console.log("Album: " + data.tracks.items[0].album.name);
-//      })
-//    }
-//  }
-
 function spotifyThis() {
-  spotify.search({type: "track", query: `${userInput}`}, function(error, data){
-    if (error){
-      console.log(error)
-    } else {
+  if (userInput === ""){
+    userInput = "The Sign"
+  }
+  spotify.search({ type: "track", query: `${userInput}`}, function (error, data){
+    if (error) {
+      console.log (error)
+    }
+    else {
       console.log("Artist: " + data.tracks.items[0].artists[0].name);
       console.log("Title: " + data.tracks.items[0].name);
       console.log("Preview: " + data.tracks.items[0].preview_url);
       console.log("Album: " + data.tracks.items[0].album.name);
     }
-  }
-  )};
+  })
+}
+
+// function spotifyThis() {
+//   spotify.search({
+//     type: "track",
+//     query: `${userInput}`
+//   }, function (error, data) {
+//     if (error) {
+//       console.log(error)
+//     } else {
+//       console.log("Artist: " + data.tracks.items[0].artists[0].name);
+//       console.log("Title: " + data.tracks.items[0].name);
+//       console.log("Preview: " + data.tracks.items[0].preview_url);
+//       console.log("Album: " + data.tracks.items[0].album.name);
+//     }
+//   })
+// };
 
 
 function omdbThis() {
-  if(userInput===""){
+  if (userInput === "") {
     axios.get(`http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy`).then(
-      function(response){
+      function (response) {
         console.log(response.data.Title);
         console.log(response.data.Year);
         console.log(response.data.Ratings[0]);
@@ -74,32 +76,31 @@ function omdbThis() {
       }
     )
   } else {
-  axios.get(`http://www.omdbapi.com/?t=${userInput}&apikey=trilogy`).then(
-    function (response) {
-      console.log(response.data.Title);
-      console.log(response.data.Year);
-      console.log(response.data.Ratings[0]);
-      console.log(response.data.Ratings[1]);
-      console.log(response.data.Country);
-      console.log(response.data.Language);
-      console.log(response.data.Plot);
-      console.log(response.data.Actors);
-    }
-  )
+    axios.get(`http://www.omdbapi.com/?t=${userInput}&apikey=trilogy`).then(
+      function (response) {
+        console.log(response.data.Title);
+        console.log(response.data.Year);
+        console.log(response.data.Ratings[0]);
+        console.log(response.data.Ratings[1]);
+        console.log(response.data.Country);
+        console.log(response.data.Language);
+        console.log(response.data.Plot);
+        console.log(response.data.Actors);
+      }
+    )
   }
 }
 
 function bitThis() {
   axios.get(`https://rest.bandsintown.com/artists/${userInput.trim()}/events?app_id=codingbootcamp`).then(
-    function(response){
-    // console.log(response)
-    for (var i = 0; i < response.data.length; i ++){
-    console.log("=============================")
-    console.log(response.data[i].venue.name);
-    console.log(response.data[i].venue.city);
-    console.log(moment(response.data[i]).format("MM-DD-YYYY"));
-    console.log("=============================")
-    }
+    function (response) {
+      for (var i = 0; i < response.data.length; i++) {
+        console.log("=============================")
+        console.log(response.data[i].venue.name);
+        console.log(response.data[i].venue.city);
+        console.log(moment(response.data[i]).format("MM-DD-YYYY"));
+        console.log("=============================")
+      }
     }
   )
 }
@@ -107,8 +108,8 @@ function bitThis() {
 function doIt() {
   fs.readFile("random.txt", "utf8", function (error, data) {
     dataArr = data.split(",");
-    request=dataArr[0];
-    userInput=dataArr[1];
+    request = dataArr[0];
+    userInput = dataArr[1];
     if (request === "spotify-this-song") {
       spotifyThis();
 
@@ -118,7 +119,7 @@ function doIt() {
     } else if (request === "concert-this") {
       console.log(userInput)
       bitThis();
-      
+
     } else {
       return console.log(error)
     }
